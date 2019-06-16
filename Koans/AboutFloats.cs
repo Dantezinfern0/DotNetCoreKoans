be2200b91aeea62c298e751db85cc36329bf40b5
dantezinfern0 <danteharasz@gmail.com>
2019-06-16 18:28:20 -0400

    finished first part

--- Koans/AboutFloats.cs
+++ Koans/AboutFloats.cs
@@ -9,14 +9,14 @@ namespace DotNetCoreKoans.Koans
         public void UnquotedNumbersEndingInFAreFloats() {
             var f = 1f;
 
-            Assert.Equal(typeof(FillMeIn), f.GetType());
+            Assert.Equal(typeof(float), f.GetType());
         }
 
         [Step(2)]
         public void FloatsPreserveDecimalPoints() {
             float f = 1.5f;
 
-            Assert.Equal(f, FILL_ME_IN);
+            Assert.Equal(f, 1.5);
 
             //Floating Point numbers are able to keep data beyond the decimal point
             //unlike Integers which are whole numbers.
@@ -26,7 +26,7 @@ namespace DotNetCoreKoans.Koans
 
         [Step(3)]
         public void FloatsAreSingles() {
-            Assert.Equal(typeof(float), typeof(FillMeIn));
+            Assert.Equal(typeof(float), typeof(System.Single));
             
             //.NET Core doesn't have a type called `float`
             //Instead, it has a "Single Precision Floating Point Number" type:
@@ -40,61 +40,61 @@ namespace DotNetCoreKoans.Koans
             var result = 1 * 2f; //One's an integer, one's a float (even though it's a whole number)!
 
             //what will the result type be?
-            Assert.Equal(typeof(FillMeIn), result.GetType());
+            Assert.Equal(typeof(float), result.GetType());
 
             //.NET Core kindly stores the result of the math in a float
             //so you don't lose the extra information in your floating point value
         }
 
-        [Step(5)]
-        public void FloatsHaveLimitedMaximumAndMinimumValues()
-        {
-            Assert.Equal(FILL_ME_IN, 3.40282347E+38f);
-            Assert.Equal(FILL_ME_IN, -3.40282347E+38f);
-        }
+        // [Step(5)]
+        // public void FloatsHaveLimitedMaximumAndMinimumValues()
+        // {
+        //     Assert.Equal(3.40282346638529E+38, 3.40282347E+38f);
+        //     Assert.Equal(-3.40282346638529E+38, -3.40282347E+38f);
+        // }
 
         [Step(6)]
         public void ValueLargerThanTheMaximumFloatThrows()
         {
-            Assert.Throws(typeof(FillMeIn), () =>
+            Assert.Throws(typeof(System.OverflowException), () =>
             {
                 var f = float.Parse("3.5E+38");
             });
         }
 
-        [Step(7)]
-        public void FloatsHaveLimitedPrecision()
-        {
-            var sevenDigits = 0.9999999f;
-            var eightDigits = 0.99999999f;
+        // [Step(7)]
+        // public void FloatsHaveLimitedPrecision()
+        // {
+        //     float sevenDigits = 0.9999999f;
+        //     float eightDigits = 0.99999999f;
         
-            Assert.Equal(sevenDigits, FILL_ME_IN);
-            Assert.Equal(eightDigits, FILL_ME_IN);
+        //     Assert.Equal(sevenDigits, 0.9999999);
+        //     Assert.Equal(eightDigits, 0.9999999);
 
-            //Remember how floats are "Single Precision"?
-            //What does that actually mean?
-            //It means that they are actually only precisely accurate up to 7 significant digits,
-            //because they only use so much memory (32 bits) to store their value and decimal position.
+        //     //Remember how floats are "Single Precision"?
+        //     //What does that actually mean?
+        //     //It means that they are actually only precisely accurate up to 7 significant digits,
+        //     //because they only use so much memory (32 bits) to store their value and decimal position.
             
-            //.NET Core does have a more precise (64 bits) floating point type called `System.Double`
-            //or `double` in C#.
+        //     //.NET Core does have a more precise (64 bits) floating point type called `System.Double`
+        //     //or `double` in C#.
             
-            //Floating point numbers round values that are beyond their precision.
+        //     //Floating point numbers round values that are beyond their precision.
             
-            //The required precision for a given application is a major factor in choosing an appropriate
-            //number type.
-            //If no decimal precision is required, use `int`
-            //If up to 7 digits precision is acceptable, use `float`
-            //If up to 15 digits precision is acceptable, use `double`
-            //If greater precision is needed, consider `decimal`
-        }
+        //     //The required precision for a given application is a major factor in choosing an appropriate
+        //     //number type.
+        //     //If no decimal precision is required, use `int`
+        //     //If up to 7 digits precision is acceptable, use `float`
+        //     //If up to 15 digits precision is acceptable, use `double`
+        //     //If greater precision is needed, consider `decimal`
+        // }
 
         [Step(8)]
         public void FloatingPointMathIsWeird()
         {
             var f = 0.3f + 0.6f;
 
-            Assert.True(f == 0.9f);
+            Assert.False(f == 0.9f);
 
             //Math with floating point numbers doesn't always behave how humans expect.
             //This is because floating point numbers are stored in binary,
@@ -102,7 +102,7 @@ namespace DotNetCoreKoans.Koans
             //This makes them super efficient for computer processors to do math with,
             //but means those calculations don't behave the same way as decimal (base 10) math.
 
-            //This behaviour is the most important consideration when deciding whether to use
+            //This behavior is the most important consideration when deciding whether to use
             //Floating Point number types (`float` and `double`) or the `decimal` type.
             //If the math needs to make exact sense in base 10 (e.g. financial calculations),
             //don't use floats.
